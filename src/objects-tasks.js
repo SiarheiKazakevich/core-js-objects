9fea9f6357763a96bce8bcb8792ce5142a8b3de0
Siarhei Kazakevich <dianrom@tut.by>
2025-03-10 00:51:53 +0300

    feat: finish task removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -54,8 +54,10 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, ['age']) => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) => !keys.includes(key))
+  );
 }
 
 /**
